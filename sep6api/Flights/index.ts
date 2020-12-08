9ad28eed94872b1dca952e17dc023a5906a7df49
Jakub Duchon <jakub.duchon9@gmail.com>
2020-12-08 11:33:30 +0100

    feat: add flights func

---
+++ sep6api/Flights/index.ts
@@ -0,0 +1,23 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { Client } from 'pg'
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    context.log('HTTP trigger function processed a request.');
+
+    const connectionString = process.env.PG_CONNECTION_STRING
+
+    const client = new Client({
+        connectionString
+    })
+    
+    client.connect()
+    const { rows } = await client.query('SELECT * FROM flights LIMIT 10;')
+    
+    context.res = {
+        status: 200, /* Defaults to 200 */
+        body: rows
+    };
+
+};
+
+export default httpTrigger;
