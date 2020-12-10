02fb0eb1dd7f359cc226126f2e63ca60d0d14d89
Jakub Duchon <jakub.duchon9@gmail.com>
2020-12-11 00:51:49 +0100

    feat(api): implement flight queries

--- sep6api/Flights/index.ts
+++ sep6api/Flights/index.ts
@@ -1,23 +1,89 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
-import { Client } from 'pg'
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import { Pool, PoolClient } from "pg";
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  _: HttpRequest
+): Promise<void> {
+  context.log("HTTP trigger function processed a request.");
 
-    const connectionString = process.env.PG_CONNECTION_STRING
+  const pool = new Pool({
+    connectionString: process.env.PG_CONNECTION_STRING,
+  });
 
-    const client = new Client({
-        connectionString
+  await pool
+    .connect()
+    .then(async (client) => {
+      const data = {
+        months: {},
+        airports: {},
+      };
+
+      data.months = await getDataPerMonth(client);
+      data.airports = await getDataPerAirport(client);
+
+      context.res = {
+        status: 200,
+        body: {
+          data,
+        },
+      };
     })
-    
-    client.connect()
-    const { rows } = await client.query('SELECT * FROM flights LIMIT 10;')
-    
-    context.res = {
-        status: 200, /* Defaults to 200 */
-        body: rows
-    };
+    .catch((err) => {
+      console.error("Unexpected error on idle client", err);
+      context.res = {
+        status: 500,
+        body: "Unexpected error when connecting to the database.",
+      };
+    });
+};
 
+async function getDataPerMonth(client: PoolClient) {
+  const data = {};
+
+  for (const month in months) {
+    data[month] = (
+      await client.query(
+        `SELECT COUNT(*) FROM flights WHERE flights.month = '${months[month]}';`
+      )
+    ).rows[0].count;
+  }
+
+  return data;
+}
+
+async function getDataPerAirport(client: PoolClient) {
+  const data = {};
+  for (const airport of airports) {
+    for (const month in months) {
+      if (!data[airport]) {
+        data[airport] = {};
+      }
+      data[airport][month] = (
+        await client.query(
+          `SELECT COUNT(*) FROM flights WHERE flights.month = '${months[month]}' AND flights.origin = '${airport}';`
+        )
+      ).rows[0].count;
+    }
+  }
+  return data;
+}
+
+const airports = ["JFK", "LGA", "EWR"];
+
+const months = {
+  january: "1",
+  february: "2",
+  march: "3",
+  april: "4",
+  may: "5",
+  june: "6",
+  july: "7",
+  august: "8",
+  september: "9",
+  october: "10",
+  november: "11",
+  december: "12",
 };
 
 export default httpTrigger;
