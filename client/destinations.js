7d71d471242e6f392567fa1452dfc730f09d77f8
Jakub Duchon <jakub.duchon9@gmail.com>
2020-12-12 11:54:03 +0100

    refactor(client): create a helper function for mapping keys/values

--- client/destinations.js
+++ client/destinations.js
@@ -1,7 +1,6 @@
 import ChartBuilder from './chartBuilder.js';
 
-const URL =
-  'https://sep6api.azurewebsites.net/api/Destinations';
+const URL = 'https://sep6api.azurewebsites.net/api/Destinations';
 
 const topDestinationsChart = new ChartBuilder(
   document.getElementById('topDestinations')
@@ -24,43 +23,43 @@ fetch(URL)
     return response.json();
   })
   .then((response) => {
-    console.log('Total:', response.total);
     topDestinationsChart
-      .setLabels(response.total.map((record) => record.dest))
-      .addDataset(
-        'TOP 10 destinations',
-        response.total.map((record) => record.count)
-      )
+      .setLabels(getValues(response.total, 'dest'))
+      .addDataset('TOP 10 destinations', getValues(response.total, 'count'))
       .toggleProgressBar()
       .build();
 
     topDestinationsEwrChart
-      .setLabels(response.airports['EWR'].map((record) => record.dest))
+      .setLabels(getValues(response.airports['EWR'], 'dest'))
       .addDataset(
         'TOP 10 destinations',
-        response.airports['EWR'].map((record) => record.count)
+        getValues(response.airports['EWR'], 'count')
       )
       .toggleProgressBar()
       .build();
 
     topDestinationsLgaChart
-      .setLabels(response.airports['LGA'].map((record) => record.dest))
+      .setLabels(getValues(response.airports['LGA'], 'dest'))
       .addDataset(
         'TOP 10 destinations',
-        response.airports['LGA'].map((record) => record.count)
+        getValues(response.airports['LGA'], 'count')
       )
       .toggleProgressBar()
       .build();
 
     topDestinationsJfkChart
-      .setLabels(response.airports['JFK'].map((record) => record.dest))
+      .setLabels(getValues(response.airports['JFK'], 'dest'))
       .addDataset(
         'TOP 10 destinations',
-        response.airports['JFK'].map((record) => record.count)
+        getValues(response.airports['JFK'], 'count')
       )
       .toggleProgressBar()
       .build();
   })
   .catch((err) => {
-    console.log('Error:', err);
+    console.log('An error occured when fetching data from the API:', err);
   });
+
+function getValues(array, key) {
+  return array.map((record) => record[key]);
+}
