845b99129eca6b9ea5970aa7086786314b906b2e
Jvergo <Jvergoa@gmail.com>
2020-12-13 10:43:02 +0100

    Weather test

--- sep6api/Weather/index.ts
+++ sep6api/Weather/index.ts
@@ -18,8 +18,7 @@ const httpTrigger: AzureFunction = async function (
       airports: {},
     };
 
-    data.total = await getTotalFlights(client);
-    data.airports = await getFlightsPerAirport(client);
+    data.total = await getWeather(client);
 
     context.res = {
       status: 200,
@@ -34,42 +33,13 @@ const httpTrigger: AzureFunction = async function (
   }
 };
 
-async function getTotalFlights(client: PoolClient) {
-  const data = {};
-
-  for (const month in months) {
-    data[month] = (
-      await client.query(
-        `SELECT COUNT(*) FROM flights WHERE flights.month = '${months[month]}';`
-      )
-    ).rows[0].count;
-  }
+async function getWeather(client: PoolClient) {
+  let data;
+  data = await client.query(
+    `SELECT * FROM weather;`
+  )
 
   return data;
 }
-
-async function getFlightsPerAirport(client: PoolClient) {
-  const data = {};
-  for (const airport of airports) {
-    for (const month in months) {
-      if (!data[airport]) {
-        data[airport] = {};
-      }
-      data[airport][month] = (
-        await client.query(
-          `SELECT COUNT(*) FROM flights WHERE flights.month = '${months[month]}' AND flights.origin = '${airport}';`
-        )
-      ).rows[0].count;
-    }
-  }
-  return data;
-}
-
-const airports = ["JFK", "LGA", "EWR"];
-// prettier-ignore
-const months = {
-  january: "1", february: "2", march: "3", april: "4", may: "5", june: "6", july: "7", 
-  august: "8", september: "9", october: "10", november: "11", december: "12",
-};
 
 export default httpTrigger;
